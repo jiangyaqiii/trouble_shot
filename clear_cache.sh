4e374d048032f29359b4a7432adb6cc6cca56f83
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-26 17:57:26 +0800

    Create clear_cache.sh

---
+++ clear_cache.sh
@@ -0,0 +1,18 @@
+# ===================================清除缓存执行过程======================================================================
+echo "================清除缓存执行过程================"
+echo ""
+echo ""
+echo "清除缓存"
+sudo rm -rf /var/log/*
+sudo rm -rf /tmp/*
+sudo apt-get remove --purge $(dpkg --get-selections l grep -v deinstall |grep -v "linux-image" |awk '{print $1}')
+sudo apt-get -y autoremove
+sudo apt-get -y clean
+cp -r /etc/skel/. /root/
+sudo rm -rf /etc/network/interfaces
+sudo cp /etc/network/interfaces.dpkg-dist /etc/network/interfaces
+sudo update-grub
+# ================================================================================================================================
+echo ""
+echo "修复完成，机器自动重启，无需操作，会自动重启相关服务"
+sudo reboot
